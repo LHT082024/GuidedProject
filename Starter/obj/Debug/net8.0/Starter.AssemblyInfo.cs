9dd2ce18bdd7863935b86393135bbe85a1521d34
LHT082024 <LHT082024@gmail.com>
2024-10-01 13:44:42 +0200

    added grades for new students

--- Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68f5e5268ba335bc626174324514057a94290a38")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+523db59550b2c40a261d4fa3b25ddc877e365694")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
